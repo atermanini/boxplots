d281ee2f8ccb476497af7a60ee1ca52154a4f23c
atermanini <termanini@me.com>
2016-07-13 17:36:33 +0200

    added log2-transformation of data

--- main.R
+++ main.R
@@ -30,7 +30,8 @@ m = matrix(c(
   "title", "t", "2", "character", "plot title (default = Boxplots)",
   "xlab", "x", "2", "character", "x-axys label (default = Groups)",
   "ylab", "y", "2", "character", "y-axys label (default = Value)",
-  "rotate-labs", "r", "0", "logical", "rotate labels in vertical position (default = false)"
+  "rotate-labs", "r", "0", "logical", "rotate labels in vertical position",
+  "log2", "l", "0", "logical", "log2 transformation"  
  ), byrow=TRUE, ncol=5);
 opt = getopt(spec = m, opt = commandArgs(TRUE));
 
@@ -46,6 +47,7 @@ if ( is.null(opt$"title") )     	{ opt$"title" = "Boxplots"; }
 if ( is.null(opt$"xlab") )      	{ opt$"xlab" = "Groups"; }
 if ( is.null(opt$"ylab") )      	{ opt$"ylab" = "Value"; }
 if ( is.null(opt$"rotate-labs" ) )	{ opt$"rotate-labs" = FALSE; }
+if ( is.null(opt$"log2" ) )			{ opt$"log2" = FALSE; }
 
 # print values:
 if (opt$"verbose"==TRUE) 			{ print(opt); }
@@ -68,8 +70,12 @@ if(file.exists(opt$"infile")) {
 }
 
 #----------------------- DATA PROCESSING:
-#if (opt$"verbose"==TRUE) { verbose("Processing data"); }
-
+if (opt$"verbose"==TRUE) { verbose("Processing data"); }
+if(opt$"log2"==TRUE) {
+	
+	data[data<=0] = min(data[data>0]);	# avoid log2 of zero or negative values
+	data = log2(data);
+}
 
 #----------------------- MAKING PLOT:
 if (opt$"verbose"==TRUE) { verbose("Making plot") };
